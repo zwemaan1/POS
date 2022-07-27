d2ae9ec3bcd38eba13436d81b65296a24f3aee94
unknown <zwemaan881@gmail.com>
2022-07-27 16:02:46 +0630

    Add Card And Left Screen Tab

--- resources/views/tailwind.blade.php
+++ resources/views/tailwind.blade.php
@@ -171,29 +171,131 @@
                             </div>
                         </div>
 
+                    </div>
 
 
-
+                    <div class="grid grid-cols-4 gap-10 p-10 pt-0">
                         
-                       
+                        <div class="max-w-xs bg-white rounded-lg border border-gray-200 shadow-md ">
+                            <a href="#">
+                                <img class="rounded-t-lg" src="img/f1.jpg" alt="">
+                            </a>
+                            <div class="p-5">
+
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Pizza</p>
+                                <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Read more
+                                    <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="max-w-xs bg-white rounded-lg border border-gray-200 shadow-md ">
+                            <a href="#">
+                                <img class="rounded-t-lg" src="img/f1.jpg" alt="">
+                            </a>
+                            <div class="p-5">
+
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Pizza</p>
+                                <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Read more
+                                    <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="max-w-xs bg-white rounded-lg border border-gray-200 shadow-md ">
+                            <a href="#">
+                                <img class="rounded-t-lg" src="img/f1.jpg" alt="">
+                            </a>
+                            <div class="p-5">
+
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Pizza</p>
+                                <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Read more
+                                    <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="max-w-xs bg-white rounded-lg border border-gray-200 shadow-md ">
+                            <a href="#">
+                                <img class="rounded-t-lg" src="img/f1.jpg" alt="">
+                            </a>
+                            <div class="p-5">
+
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Pizza</p>
+                                <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Read more
+                                    <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </a>
+                            </div>
+                        </div>
+                        
                     </div>
 
                     
                 </div>    
     <!-- Card End -->
 
-                </div>
-<!-- Left Screen End -->
+                    <!-- Pagination Start -->
+                    <div class="flex flex-col items-center">
+                        
+                        <span class="text-sm text-gray-700 dark:text-gray-400">
+                            Showing <span class="font-semibold text-gray-900 dark:text-white">1</span> to <span class="font-semibold text-gray-900 dark:text-white">10</span> of <span class="font-semibold text-gray-900 dark:text-white">100</span> Entries
+                        </span>
+                        <div class="inline-flex mt-2 xs:mt-0">
+                           
+                            <button class="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800">
+                                <svg aria-hidden="true" class="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>
+                                Prev
+                            </button>
+                            <button class="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-r border-0 border-l border-gray-700 hover:bg-gray-900 dark:bg-gray-800">
+                                Next
+                                <svg aria-hidden="true" class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- Pagination End  -->
 
-                <div class="max-h-fit bg-stone-500 col-span-4">
-                    
                 </div>
+                <!-- Left Screen End -->
+
+
+
+                <!-- Right Screen Start -->
+                <div class="max-h-fit bg-stone-500 col-span-4 p-7">
+                    <div class="bg-white rounded-lg p-4">
+                        <h1 class="font-semibold">New Order List</h1>
+                        <hr>
+                        <div class="mt-5">
+                            <div class="bg-blue-700 flex px-5 rounded-lg">
+                                <div class="p-2">
+                                    <img class="rounded-t-lg w-20 h-20 rounded-lg" src="img/f1.jpg" alt="IMG" />
+                                </div>
+                                <div class="flex flex-col">
+                                    <h1 class="text-white font-bold font-2xl">Pizza</h1>
+                                    <div class="mt-4">
+                                        <button class="bg-white rounded-full w-7 h-5">1</button>
+                                           
+                                        <button class="bg-white rounded-lg w-10 h-5 text-center">1</button>
+
+                                        <button class="bg-white rounded-full w-7 h-5">1</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Right Screen End -->
+
             </div>
-<!-- left And Right div End -->
+            <!-- left And Right div End -->
 
         </div>
         
-   </div><!-- flex end -->
+   </div>
+   <!-- flex end -->
 
 
 </body>
